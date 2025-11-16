834a547045e08d570a07de00c090a8529324790c
KMF <kjellmf@gmail.com>
2025-11-16 06:59:58 +0100

    Refactor map setup

--- src/main.js
+++ src/main.js
@@ -4,34 +4,14 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import { GlobeControl, Map as MlMap, NavigationControl } from "maplibre-gl";
 import ms from "milsymbol";
 
-const map = new MlMap({
-  container: "map",
-  // style: "https://demotiles.maplibre.org/globe.json",
-  style: "https://tiles.openfreemap.org/styles/positron",
-  center: [0, 0],
-  zoom: 2,
-});
+// Tweak these values to see different effects
+let numberOfFeatures = 500;
+let radius = 40; //
+let timeFactor = 100000; // higher value = slower animation
+const pixelRatio = 2; // used when converting MIL-STD-2525D symbols to images
 
-map.addControl(new GlobeControl());
-map.addControl(
-  new NavigationControl({
-    visualizePitch: true,
-    visualizeRoll: true,
-    showZoom: true,
-    showCompass: true,
-  }),
-);
-
-map.on("style.load", () => {
-  map.setProjection({
-    type: "globe", // Set projection to globe
-  });
-});
-
-const numPoints = 500;
-const radius = 40;
-
-const iconUrls = {
+// Icon names that starts with "mil-" are MILSTD2525D/APP6-D symbol identification codes
+const iconData = {
   // horse: "https://maps.google.com/mapfiles/kml/shapes/horsebackriding.png",
   // airport2: "https://maps.google.com/mapfiles/kml/shapes/airports.png",
   // volcano2: "https://maps.google.com/mapfiles/kml/shapes/volcano.png",
@@ -42,9 +22,76 @@ const iconUrls = {
   "mil-4": "10041500001106030000",
 };
 
-const icons = Object.keys(iconUrls);
+const iconNames = Object.keys(iconData);
+const map = await setupMap();
 
-function pointOnCircle(angle, radius = 20, icon = "airport") {
+async function setupMap() {
+  const map = new MlMap({
+    container: "map",
+    // style: "https://demotiles.maplibre.org/globe.json",
+    style: "https://tiles.openfreemap.org/styles/positron",
+    center: [0, 0],
+    zoom: 2,
+  });
+
+  map.addControl(new GlobeControl());
+  map.addControl(
+    new NavigationControl({
+      visualizePitch: true,
+      visualizeRoll: true,
+      showZoom: true,
+      showCompass: true,
+    }),
+  );
+
+  for (const [icon, url] of Object.entries(iconData)) {
+    if (icon.startsWith("mil-")) {
+      const data = createMilsymbolImage(url, {
+        pixelRatio,
+        options: {
+          size: 20,
+          outlineWidth: 10,
+          outlineColor: "yellow",
+        },
+      });
+      if (data && !map.hasImage(icon)) map.addImage(icon, data, { pixelRatio });
+    } else {
+      const image = await map.loadImage(url);
+      if (image && !map.hasImage(icon)) map.addImage(icon, image.data);
+    }
+  }
+
+  map.on("style.load", () => {
+    map.setProjection({
+      type: "globe",
+    });
+
+    map.addSource("point", {
+      type: "geojson",
+      data: { type: "FeatureCollection", features: createFeatures(0) },
+    });
+
+    map.addLayer({
+      id: "point",
+      source: "point",
+      type: "symbol",
+      // https://maplibre.org/maplibre-style-spec/layers/#symbol
+      layout: {
+        "icon-image": ["get", "icon"],
+        "icon-rotate": ["get", "bearing"],
+        "icon-rotation-alignment": "map",
+        "icon-overlap": "always",
+        "icon-ignore-placement": true,
+        "icon-size": 1,
+        // "icon-size": 0.5,
+      },
+    });
+  });
+
+  return map;
+}
+
+function pointOnCircle(angle, radius, icon) {
   return {
     type: "Feature",
     properties: { icon, bearing: 0 },
@@ -56,72 +103,34 @@ function pointOnCircle(angle, radius = 20, icon = "airport") {
 }
 
 function createFeatures(timestamp) {
-  return Array.from({ length: numPoints }, (_, i) =>
+  return Array.from({ length: numberOfFeatures }, (_, i) =>
     pointOnCircle(
-      ((i / numPoints) * 2 * Math.PI * timestamp) / 100000,
+      ((i / numberOfFeatures) * 2 * Math.PI * timestamp) / timeFactor,
       i * radius * 0.1,
-      icons[i % icons.length],
+      iconNames[i % iconNames.length],
     ),
   );
 }
 
-function createMilsymbolImage(iconId, sidc, options = {}) {
+function createMilsymbolImage(sidc, { pixelRatio = 2, options = {} } = {}) {
   const symb = new ms.Symbol(sidc, {
     size: 20,
     ...options,
   });
   const { width, height } = symb.getSize();
-  const data = symb
-    .asCanvas(2)
+  return symb
+    .asCanvas(pixelRatio)
     ?.getContext("2d")
-    ?.getImageData(0, 0, 2 * width, 2 * height);
-  if (data && !map.hasImage(iconId)) {
-    map.addImage(iconId, data, { pixelRatio: 2 });
-  }
+    ?.getImageData(0, 0, pixelRatio * width, pixelRatio * height);
 }
 
-function animateMarkers(timestamp) {
+function animateFeatures(timestamp) {
   const features = createFeatures(timestamp);
   map.getSource("point")?.setData({ type: "FeatureCollection", features });
-  requestAnimationFrame(animateMarkers);
+  requestAnimationFrame(animateFeatures);
 }
 
 map.on("load", async () => {
-  // add icons to map
-  let image;
-  for (const [icon, url] of Object.entries(iconUrls)) {
-    if (icon.startsWith("mil-")) {
-      createMilsymbolImage(icon, url, {
-        size: 20,
-        outlineWidth: 10,
-        outlineColor: "yellow",
-      });
-    } else {
-      image = await map.loadImage(url);
-      if (image && !map.hasImage(icon)) map.addImage(icon, image.data);
-    }
-  }
-
-  map.addSource("point", {
-    type: "geojson",
-    data: { type: "FeatureCollection", features: createFeatures(0) },
-  });
-
-  map.addLayer({
-    id: "point",
-    source: "point",
-    type: "symbol",
-    layout: {
-      "icon-image": ["get", "icon"],
-      "icon-rotate": ["get", "bearing"],
-      "icon-rotation-alignment": "map",
-      "icon-overlap": "always",
-      "icon-ignore-placement": true,
-      "icon-size": 1,
-      // "icon-size": 1,
-    },
-  });
-
   // Start the animation.
-  animateMarkers(0);
+  animateFeatures(0);
 });
